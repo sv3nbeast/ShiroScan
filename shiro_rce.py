cc3c22343c4b7bf8c0f52863a778c1e35306e01c
斯文beast <55974091+sv3nbeast@users.noreply.github.com>
2020-03-12 17:37:25 +0800

    Update shiro_rce.py

--- shiro_rce.py
+++ shiro_rce.py
@@ -21,7 +21,7 @@ print('Welcome To Shiro反序列化 RCE ! ')
 if __name__ == '__main__':
     if len(sys.argv)<2:
         print("Usage:"+"python3 shiro.py  url  command")
-        print("Usage:"+"若import模块错误，安装不成功，请到linux系统安装运行，或者去python库将crypto首字母改为大写")
+        print("Usage:"+"若import模块错误，安装不成功，请到linux系统安装运行，或者去python库将crypto首字母改为大写并尝试pip install pycryptodome")
         print('Usage：python3 shiro.py  http://url.com  "ping dnslog.cn"   注意命令用""包起来')
     else:
         url = sys.argv[1]
